ad633d28799e919ee72e4e66fb5086420d253433
selarab <selarab@uncc.edu>
2015-11-05 01:43:19 +0000

    hw 2 part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,55 @@ class MoviesController < ApplicationController
   end
 
   def index
+    
+    @redirect = false
+    @all_ratings = Movie.all_ratings
     @movies = Movie.all
+    
+    
+    if params[:ratings]
+      @ratings = params[:ratings]
+    elsif session[:ratings]
+      @ratings = session[:ratings]
+      @redirect = true
+    else
+      @ratings = { 'G' => '1', 'PG' => '1', 'PG-13' => '1', 'R' => '1', 'NC-17' => '1' }
+    end
+    
+    @selected = @ratings.keys
+    
+    if params[:sort]
+      @sort = params[:sort]
+    elsif session[:sort]
+      @sort = session[:sort]
+      @redirect = true
+    else
+      @sort = 'title'
+    end
+    
+    if @redirect
+      redirect_to movies_path(:sort => @sort, :ratings => @ratings)
+    end
+    
+    @movies = @movies.select{ |movie| @ratings.has_key?(movie.rating) }
+    
+    if params[:commit] == 'Refresh'
+      session[:ratings] = params[:ratings]
+    end
+    
+    
+    if @sort == 'title'
+      @movies = @movies.sort_by{ |movie| movie.title }
+    elsif @sort == 'release_date'
+      @movies = @movies.sort_by{ |movie| movie.release_date }
+    end
+    
+    instance_eval %Q"
+      @hilite_#{params[:sort]} = true
+    "
+    
+    session[:sort] = @sort
+    session[:ratings] = @ratings
   end
 
   def new
